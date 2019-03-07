09c1888e51bbe32d0658b9b12dc8950545f4988b
Estefany Rueda <yurley.rueda.2019@upb.edu.co>
2019-03-07 18:07:24 -0500

    Modificacion ejercicio 2

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,9 +1,13 @@
-num=1
-
-while num<=20:
-	num=num+1
-	if num%2==0:
-		print(num)
+contar_pares=0
+contar_impares=0
+for i in range(0,10):
+	num=int(input("Ingrese un número: "))
+ 	if %2==0:
+ 		contar_pares+=1
+ 	else:
+ 		if %2!=0:
+ 		contar_impares+=1
+print("El número de pares es", contar_pares, "El número de impares es", contar_impares)
 		
 	
 
